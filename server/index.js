3c756507db31f22d75462feb617d5bc3a7520111
yuchuan <yuchuan@yuchuan.com>
2019-04-24 14:33:03 -0400

    Add basic server setup, rental routes, MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+
+const Rental = require('./models/rental');
+
+const rentalRoutes = require('./routes/rentals');
+
+mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
+    const fakeDb = new FakeDb();
+
+    fakeDb.seedDb();
+});
+
+const app = express();
+
+app.use('/api/v1/rentals', rentalRoutes);
+
+// app.get('/rentals', (req, res) => {
+//     res.json({ 'success': true });
+// });
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log('App is running on port 3001');
+});
